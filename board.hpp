fe8a3a2afa818ea7ff04871f93d5f4bfdcf09a26
Dmitry Vyal <akamaus@gmail.com>
2014-03-18 00:59:22 +0400

    basic move elements

--- board.hpp
+++ board.hpp
@@ -32,42 +32,50 @@ public:
     }
 
     void Move(Turn t) {
-        for (uint m=0; m<size; m++) {
-            for (uint n=0; n<size-1; n++) {
-                uint cur_i, cur_j, next_i, next_j;
-                switch(t) {
-                case Turn::Left:
-                    cur_i = m; cur_j = n;
-                    next_i = m, next_j = n+1;
-                    break;
-                case Turn::Right:
-                    cur_i = m; cur_j = size-1-n;
-                    next_i = m, next_j = size-1-(n+1);
-                    break;
-                case Turn::Up:
-                    cur_i = n; cur_j = m;
-                    next_i = n+1, next_j = m;
-                    break;
-                case Turn::Down:
-                    cur_i = size-1-n; cur_j = m;
-                    next_i = size-1-(n+1), next_j = m;
-                    break;
-                };
-                value &cur(At(cur_i, cur_j));
-                value &next(At(next_i, next_j));
-                if (next == 0)
-                    continue;
-                if (cur == 0) {
-                    cur = next;
-                    next = 0;
-                    continue;
-                }
-                if (cur == next) {
-                    cur++;
-                    next = 0;
-                    continue;
+        bool changed = false;
+        for (uint k=0; k<size; k++) {
+            for (uint m=0; m<size; m++) {
+                for (uint n=0; n<size-1; n++) {
+                    uint cur_i, cur_j, next_i, next_j;
+                    switch(t) {
+                    case Turn::Left:
+                        cur_i = m; cur_j = n;
+                        next_i = m, next_j = n+1;
+                        break;
+                    case Turn::Right:
+                        cur_i = m; cur_j = size-1-n;
+                        next_i = m, next_j = size-1-(n+1);
+                        break;
+                    case Turn::Up:
+                        cur_i = n; cur_j = m;
+                        next_i = n+1, next_j = m;
+                        break;
+                    case Turn::Down:
+                        cur_i = size-1-n; cur_j = m;
+                        next_i = size-1-(n+1), next_j = m;
+                        break;
+                    };
+                    value &cur(At(cur_i, cur_j));
+                    value &next(At(next_i, next_j));
+                    if (next == 0)
+                        continue;
+                    if (cur == 0 && next != 0) {
+                        cur = next;
+                        next = 0;
+                        changed |= true;
+                        continue;
+                    }
+                    if (cur != 0 && cur == next) {
+                        cur++;
+                        next = 0;
+                        changed |= true;
+                        continue;
+                    }
                 }
             }
+            if (!changed)
+                break;
+
         }
     }
 
