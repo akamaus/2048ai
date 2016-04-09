490f98090c31717b21e696a5b35a3b3e8e0a69bf
Dmitry Vyal <akamaus@gmail.com>
2016-04-09 19:16:19 +0300

    calculating average

--- lua/montecarlo.lua
+++ lua/montecarlo.lua
@@ -2,14 +2,22 @@ package.path = package.path .. ';./?.lua'
 
 local Board = require 'board'
 
-function gen_episode(b)
+function table.fold(t, f, s0)
+   local s = s0 or 0
+   for i,v in ipairs(t) do
+      s = f(s, v)
+   end
+   return s
+end
+
+function gen_episode(b, policy)
    local counter = 0
    repeat
 --      print("*************")
       b:RandomGen()
 --      b:Print()
 --      print("")
-      local move = math.random(4)
+      local move = policy(b)
       b:Move(move)
 --      b:Print()
       local finish = b:IsTerminal()
@@ -18,17 +26,21 @@ function gen_episode(b)
    return counter
 end
 
+function sample_episodes(N, policy)
+   local tab = {}
 
-local tab = {}
+   for i=1,N do
+      local b = Board.new()
+      local n = gen_episode(b, policy)
+      tab[#tab + 1] = n
+   end
 
-for i=1,10000 do
-   local b = Board.new()
-   local n = gen_episode(b)
-   tab[#tab + 1] = n
+   print("avg", table.fold(tab, function(a,b) return a+b end) / N)
+
+   local GP = require 'gnuplot'
+   GP.hist(torch.Tensor(tab))
 end
 
-local GP = require 'gnuplot'
-GP.hist(torch.Tensor(tab))
+local N = 10000
 
---P.plot_table(tab)
-
+sample_episodes(N, function() return math.random(4) end)
